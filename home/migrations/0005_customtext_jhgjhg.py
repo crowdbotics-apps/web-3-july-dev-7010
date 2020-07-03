3111c7ab485c1c975b210c62459b1012a75ca4ae
Crowdbotics <team@crowdbotics.com>
2020-07-03 07:38:15 +0000

    Update models

--- home/migrations/0005_customtext_jhgjhg.py
+++ home/migrations/0005_customtext_jhgjhg.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0004_remove_customtext_jhgjhg'),
+        ("home", "0004_remove_customtext_jhgjhg"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='customtext',
-            name='jhgjhg',
+            model_name="customtext",
+            name="jhgjhg",
             field=models.GenericIPAddressField(blank=True, null=True, unpack_ipv4=True),
         ),
     ]
